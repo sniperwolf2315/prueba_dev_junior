b843a0b5a587b5ec4b6132f9dd37bb865ad730ba
sniperwolf2315 <carvilf@gmail.com>
2022-07-14 17:37:41 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Agenda de contactos web</title>
+	<link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<?php require_once "dependencias.php"; ?>
+</head>
+<body>
+	<div class="container">
+		<?php require_once "menu.php"; ?>
+
+		<div class="jumbotron">
+			<h1 class="display-4">Agenda de contactos con PHP y MySQL</h1>
+
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<img src="public/img/logo.png" class="img-fluid" alt="Responsive image">
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<table class="table table-hover">
+								<tr>
+									<th>
+										Agenda desarrollada pruevas desarrollador junior
+									</th>
+								</tr>
+								<tr>
+									<th>
+										realizado por carlos villalobos
+									</th>
+								</tr>
+								<tr>
+									<th>
+										Hecho con php y mysql
+									</th>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr class="my-4">
+			<p>
+				Agenda de contactos prueva desarrrollo junior
+			</p>
+			
+		</div>
+
+	</div>
+
+	
+</body>
+</html>
